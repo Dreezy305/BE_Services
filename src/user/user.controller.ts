ae54a87128dc818195161562666c36d0bb669c12
Dreezy <bankoleidris@gmail.com>
2022-08-26 07:33:29 +0000

    guards, sign jwt, req, user, custom decorators

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
 import { jwtGuard } from 'src/auth/guard';
 
+@UseGuards(jwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(jwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User, @GetUser('email') email: string) {
+    console.log({ email });
+    return user;
+  }
+
+  @Get('singleUser/:id')
+  getSingleUser(@GetUser('id') id: string) {
+    console.log(id);
+    return id;
   }
 }
 
