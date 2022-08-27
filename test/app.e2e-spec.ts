aaf09d9dafe71af52a0418e43ba2f4c53b3c0736
Dreezy <bankoleidris@gmail.com>
2022-08-27 15:14:16 +0000

    users, pactum e2e test, added watch, test scripts, cascade, wipe db

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,12 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -11,6 +14,10 @@ describe('App e2e', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+
+    prisma = app.get(PrismaService);
+
+    await prisma.wipeDb();
   });
 
   afterAll(() => {
