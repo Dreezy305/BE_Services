e0841cc60e271cb18774c1e033f03d30f1bd9e36
Dreezy <bankoleidris@gmail.com>
2022-08-28 16:17:00 +0000

    user service, bookmark service, bookmark dto

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,15 +1,52 @@
-import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { GetUser } from 'src/auth/decorator';
 import { jwtGuard } from '../auth/guard';
+import { BookmarkService } from './bookmark.service';
 
 @UseGuards(jwtGuard)
 @Controller('bookmarks')
 export class BookmarkController {
+  constructor(private bookmarkService: BookmarkService) {}
+
+  @Post()
+  createBookmark(@GetUser('id') userId: string) {
+    console.log('create bookmarks by id');
+  }
+
   @Get()
-  getBookmarks() {}
+  getBookmarks() {
+    console.log('bookmarks');
+  }
 
   @Get('bookmark/:id')
-  getBookmarkById() {}
+  getBookmarkById(
+    @GetUser('id') userId: string,
+    @Param('id') bookmarkId: string,
+  ) {
+    console.log('bookmarks  by id');
+  }
 
   @Patch('bookmark/:id')
-  editBookmarkById() {}
+  editBookmarkById(
+    @GetUser('id') userId: string,
+    @Param('id') bookmarkId: string,
+  ) {
+    console.log('edit bookmarks by id');
+  }
+
+  @Delete('bookmark/:id')
+  deleteBookmarkById(
+    @GetUser('id') userId: string,
+    @Param('id') bookmarkId: string,
+  ) {
+    console.log('delete bookmarks by id');
+  }
 }
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+  createBookmark() {
+    console.log('create bookmark');
+  }
+
+  getBookmarks() {
+    console.log('bookmarks');
+  }
+
+  getBookmarkById() {
+    console.log('bookmarks  by id');
+  }
+
+  editBookmarkById() {
+    console.log('edit bookmarks by id');
+  }
+
+  deleteBookmarkById() {
+    console.log('delete bookmarks by id');
+  }
+}
